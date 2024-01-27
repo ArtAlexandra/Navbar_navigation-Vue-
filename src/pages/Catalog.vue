3c9f8bbcf51ea73b4a8c1b70e42a7040d73573c6
ArtAlexandra <128704188+ArtAlexandra@users.noreply.github.com>
2024-01-27 11:38:31 +0300

    Add files via upload

---
+++ src/pages/Catalog.vue
@@ -0,0 +1,15 @@
+<template>
+    <p>CATALOG</p>
+    <button @click="$router.back()">Назад</button>
+    </template>
+    <script>
+    export default{
+        data(){
+            return{
+                
+            }
+        }
+    }
+    </script>
+    <style>
+    </style>
